17ef805a83bf7819446e79495cfc3db457e22411
danil-torovin <dantorovin@mail.ru>
2025-11-14 13:10:42 +0700

    Update vote.js

--- api/vote.js
+++ api/vote.js
@@ -3,14 +3,11 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
   if (req.method === "OPTIONS") return res.status(200).end();
-
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-
   try {
     console.log("📩 Входящий запрос:", req.body);
-
+    console.log("Body sent to Apps Script:", req.body);
     const response = await fetch(
       "https://script.google.com/macros/s/AKfycbyRGLLI8-uxZ6LqA4JvS1HytOniKZV7HoP8QniGRwIj7r-rx7-nHTaIEmRyjtXG9moEmQ/exec",
       {
@@ -19,11 +16,8 @@ export default async function handler(req, res) {
         body: JSON.stringify(req.body),
       }
     );
-
     const text = await response.text();
-
     console.log("✅ Ответ от Apps Script:", text);
-
     res.status(200).send(text);
   } catch (error) {
     console.error("❌ Ошибка:", error);
